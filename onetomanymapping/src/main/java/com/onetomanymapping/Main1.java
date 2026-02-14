b0b713c5797d9da96caa284512005573226998cb
Balepalli Pavan Kumar <119617462+BalepalliPavanKumar@users.noreply.github.com>
2026-02-14 12:31:43 +0530

    OnetoMany Mapping Relation

---
+++ onetomanymapping/src/main/java/com/onetomanymapping/Main1.java
@@ -0,0 +1,45 @@
+package com.onetomanymapping;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main1 {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		College c=em.find(College.class,1);
+		Student s1=new Student();
+		s1.setStu_Id(106);
+		s1.setName("Arjun");
+		s1.setBranch("BIO");
+		
+		List<Student> list=new ArrayList<Student>();
+		
+		list=c.getStu();
+		list.add(s1);
+		
+		c.setStu(list);
+		
+		et.begin();
+		em.persist(s1);
+		et.commit();
+		em.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
